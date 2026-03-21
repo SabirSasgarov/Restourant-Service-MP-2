89e81b8db4bf5b206547720f17c996e8bda43cbd
SabirA <sabirsas@code.edu.az>
2026-03-21 20:02:19 +0400

    first look

---
+++ RestourantServiceApp.PL/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RestourantServiceApp.PL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Works just fine!");
+
+        }
+    }
+}
