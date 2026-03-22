909d6993904f932d00468c643333e247bfddc245
SabirA <sabirsas@code.edu.az>
2026-03-22 20:21:48 +0400

    Added menuitem service

---
+++ RestourantServiceApp.BLogicLayer/Services/MenuItemService.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestourantServiceApp.Core.Enums;
+using RestourantServiceApp.Core.Models;
+using RestourantServiceApp.DataAccsessLayer.Contexts;
+using System.ComponentModel;
+
+namespace RestourantServiceApp.BLogicLayer.Services
+{
+	public class MenuItemService(RestourantDbContext context)
+	{
+		public async Task<List<MenuItem>> GetMenuItems()
+		=> await context.MenuItems
+				.AsNoTracking()
+				.ToListAsync();
+
+		public async Task AddMenuItem(string name, decimal price, Category category)
+		{
+			var newMenuItem = new MenuItem
+			{
+				Name = name,
+				Price = price,
+				Category = category
+			};
+			context.MenuItems.Add(newMenuItem);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task RemoveMenuItem(Guid menuItemId)
+		{
+			var menuItem = await context.MenuItems
+				.FirstOrDefaultAsync(mi => mi.Id == menuItemId);
+
+			if (menuItem == null)
+				throw new InvalidOperationException("Menu item not found.");
+
+			context.MenuItems.Remove(menuItem);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task EditMenuItem(Guid menuItemId, string name, decimal price)
+		{
+			var menuItem = await context.MenuItems
+				.FirstOrDefaultAsync(mi => mi.Id == menuItemId);
+
+			if (menuItem == null)
+				throw new InvalidOperationException("Menu item not found.");
+			
+			menuItem.Name = name;
+			menuItem.Price = price;
+
+			context.MenuItems.Update(menuItem);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task<List<MenuItem>> GetMenuItemsByCategory(Category category)
+		=> await context.MenuItems
+			.Where(mi => mi.Category == category)
+			.AsNoTracking()
+			.ToListAsync();
+
+		public async Task<List<MenuItem>> GetMenuItemsInRange(decimal startPrice, decimal finalPrice)
+		=> await context.MenuItems
+			.Where(mi => mi.Price >= startPrice && mi.Price<=finalPrice)
+			.AsNoTracking()
+			.ToListAsync();
+
+		public async Task<List<MenuItem>> GetMenuItemsBySearch(string search)
+		{
+			if(string.IsNullOrWhiteSpace(search))
+				return await context.MenuItems
+					.AsNoTracking()
+					.ToListAsync();
+
+			search = search.ToLower();
+			var menuItems = await context.MenuItems
+				.Where(mi => mi.Name.ToLower().Contains(search))
+				.AsNoTracking()
+				.ToListAsync();
+
+			return menuItems;
+		}
+
+	}
+}
---
+++ RestourantServiceApp.BLogicLayer/Services/OrderService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestourantServiceApp.Core.Models;
+using RestourantServiceApp.DataAccsessLayer.Contexts;
+
+namespace RestourantServiceApp.BLogicLayer.Services
+{
+	public class OrderService(RestourantDbContext context)
+	{
+		public async Task<List<Order>> GetOrders()
+			=> await context.Orders
+				.AsNoTracking()
+				.ToListAsync();
+
+		public
+
+	}
+}
